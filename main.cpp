3482055df86448aa937cb5eb87cafd5ccb58060b
Igor Benedet <benedetigor@gmail.com>
2020-11-30 19:24:23 -0300

    sdl2 e sdl image adicionados

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <QCoreApplication>
 #include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    std::cout << "ola"<< std::endl;
+    cout << "ola"<< endl;
 
     return a.exec();
 }
